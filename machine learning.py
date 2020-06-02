3e9825dd2eb172db5d04b4dcb937a2a590240fea
Vitorhugo17 <martinsvitor11@gmail.com>
2020-06-02 18:36:21 +0100

    first commit

---
+++ machine learning.py
@@ -0,0 +1,70 @@
+# imports
+import pandas as pd
+import numpy as np
+import warnings
+import flask
+from flask import request, Response, jsonify
+import json
+
+warnings.filterwarnings('ignore')
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['POST'])
+def home():
+    if 'idUtilizador' in request.json:
+        id = int(request.json['idUtilizador'])
+        dataset = pd.read_json(json.dumps(request.json['data']))
+    else:
+        return Response(json.dumps({"message": "idUtilizador not included"}), status="400",  mimetype='application/json')
+
+    quantidadeComprada = pd.DataFrame(dataset.groupby(['idUtilizador', 'idProduto'])['quantidade'].mean())
+
+    quantidadeComprada['quantidadeCompras'] = dataset.groupby(['idUtilizador', 'idProduto'])['quantidade'].count()
+
+    quantidadeComprada = quantidadeComprada.reset_index('idProduto')
+
+    matrix = dataset.pivot_table(index="idUtilizador", columns="idProduto", values="quantidade")
+
+    quantidadeComprada = quantidadeComprada.sort_values('quantidadeCompras', ascending=False)
+
+    index = quantidadeComprada.index
+
+    index = list(dict.fromkeys(index))
+
+    resultado = {}
+    
+    for x in index:
+        if x == id:
+            idProduto = quantidadeComprada.at[x, 'idProduto']
+            if isinstance(idProduto, int):
+                user_rating = matrix[idProduto]
+
+                similar = matrix.corrwith(user_rating)
+
+                correlation = pd.DataFrame(similar, columns=['correlation'])
+                correlation.dropna(inplace=True)
+                correlation = correlation.join(quantidadeComprada['quantidadeCompras'])
+                correlation.sort_values('correlation', ascending=False)
+                correlation = correlation.reset_index()
+                
+                resultado = {'idUtilizador': x, 'idProduto': correlation['index'].iloc[1], 'certeza': correlation['correlation'].iloc[1]}
+            else:
+                user_rating = matrix[idProduto]
+
+                similar = matrix.corrwith(user_rating)
+
+                correlation = pd.DataFrame(similar, columns=['correlation'])
+                correlation.dropna(inplace=True)
+                correlation = correlation.join(quantidadeComprada['quantidadeCompras'])
+                correlation.sort_values('correlation', ascending=False)
+                correlation = correlation.reset_index()
+        
+                resultado = {'idUtilizador': int(x), 'idProduto': int(correlation['index'].iloc[1]), 'certeza': float(correlation['correlation'].iloc[1])}
+                    
+    return Response(json.dumps(resultado),  mimetype='application/json')
+
+app.run()
