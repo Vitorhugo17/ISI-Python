8b0c4214524bb549a38e20d5ee01c52c469fa142
Vitorhugo17 <martinsvitor11@gmail.com>
2020-06-02 19:01:13 +0100

    alt

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def home():
                     
     return Response(json.dumps(resultado),  mimetype='application/json')
 
-app.run()
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
