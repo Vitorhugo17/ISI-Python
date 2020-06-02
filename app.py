b0560b6f3223af0515cd633e2ce1898725a5cc7a
Vitorhugo17 <martinsvitor11@gmail.com>
2020-06-02 19:16:19 +0100

    alt

--- app.py
+++ app.py
@@ -2,15 +2,13 @@
 import pandas as pd
 import numpy as np
 import warnings
-import flask
-from flask import request, Response, jsonify
+from flask import Flask, request, Response
 import json
 
 warnings.filterwarnings('ignore')
 
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
